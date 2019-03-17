b6059f4930b4e89010b7902c093959a2a20fd828
Julia Komeristaya <juliyakomeristaya@gmail.com>
2019-03-17 18:01:08 +0200

    3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const _ = require("lodash");
 
 
-class test{
+class git_call_test{
    constructor(url){
      if (!_.isString(url)){
        throw Error("url must be as String")
@@ -14,10 +14,10 @@ class test{
 
    async newTask(conv_id, obj){
       return axios.post(this.url, {"ops":[{"type":"create",conv_id,obj,"action":"user",
-        "data":{"url":"https://www.corezoid.com/api/1/json/public/524878/35e222c0a43bd7a717937f44e471fc050805d0b4","conv_id":"524878","obj":"task"},"ref":randomInteger(123456, 999999)}]})
+        "data":{"url":"https://www.corezoid.com/api/1/json/public/524878/35e222c0a43bd7a717937f44e471fc050805d0b4","conv_id":"524878","obj":"task"},"ref":12345}]})
    }
 
 }
 
 
-module.exports = test;
+module.exports = git_call_test;
